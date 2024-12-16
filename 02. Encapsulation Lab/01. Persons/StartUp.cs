eeca3433ef1277749ff359e24480d00c7f39e85e
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-12-16 20:55:44 +0200

    Encapsulation first task uploaded.

---
+++ 02. Encapsulation Lab/01. Persons/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+
+            List<Person> people = new();
+
+            for (int i = 0; i < peopleCount; i++)
+            {
+                string line = Console.ReadLine();
+                string[] tokens = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string firstName = tokens[0];
+                string lastName = tokens[1];
+                int age = int.Parse(tokens[2]);
+
+                Person person = new(firstName, lastName, age);
+
+                people.Add(person);
+            }
+
+            people.OrderBy(x => x.FirstName)
+                .ThenBy(x => x.Age)
+                .ToList()
+                .ForEach(x => Console.WriteLine(x.ToString()));
+        }
+    }
+}
