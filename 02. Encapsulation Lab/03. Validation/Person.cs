d2daf32de4479fd967c480b6edeaae9eb200cc35
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-12-16 20:57:03 +0200

    Task 3 uploaded.

---
+++ 02. Encapsulation Lab/03. Validation/Person.cs
@@ -0,0 +1,84 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        private const string FirstNameErrorMsg = "First name cannot contain fewer than 3 symbols!";
+        private const string LastNameErrorMsg = "Last name cannot contain fewer than 3 symbols!";
+        private const string AgeErrorMsg = "Age cannot be zero or a negative integer!";
+        private const string SalaryErrorMsg = "Salary cannot be less than 650 leva!";
+
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException(FirstNameErrorMsg);
+                }
+
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException(LastNameErrorMsg);
+                }
+
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(AgeErrorMsg);
+                }
+
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+            private set
+            {
+                if (salary < 650)
+                {
+                    throw new ArgumentException(SalaryErrorMsg);
+                }
+
+                salary = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+    }
+}
---
+++ 02. Encapsulation Lab/03. Validation/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string inputLine = Console.ReadLine();
+                string[] tokens = inputLine.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string firstName = tokens[0];
+                string lastName = tokens[1];
+                int age = int.Parse(tokens[2]);
+                decimal salary = decimal.Parse(tokens[3]);
+
+                try
+                {
+                    Person person = new(firstName, lastName, age, salary);
+                    Console.WriteLine(person);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
