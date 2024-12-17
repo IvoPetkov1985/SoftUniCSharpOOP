a85f7290560d9ff89b0bdb4d1eb94df6fb1e12a4
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-12-17 11:39:12 +0200

    First three tasks uploaded - inheritance.

---
+++ 01. Inheritance Lab/03. Hierarchical Inheritance/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new();
+            dog.Eat();
+            dog.Bark();
+
+            Cat cat = new();
+            cat.Eat();
+            cat.Meow();
+        }
+    }
+}
