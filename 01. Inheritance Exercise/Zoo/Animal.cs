7c281df1ac48b9f33f64393b3014b0f3c0f4e3c2
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-12-18 11:01:42 +0200

    Tasks Restaurant and Zoo uploaded.

---
+++ 01. Inheritance Exercise/Zoo/Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace Zoo
+{
+    public abstract class Animal
+    {
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
