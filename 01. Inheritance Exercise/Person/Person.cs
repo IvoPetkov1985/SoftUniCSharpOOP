f0c0e29731556c9bd11e183659ece6bdbf14f309
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-12-18 10:59:38 +0200

    First task (Person) of the exercise uploaded.

---
+++ 01. Inheritance Exercise/Person/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age must be positive!");
+                }
+
+                age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name} -> Name: {Name}, Age: {Age}";
+        }
+    }
+}
---
+++ 01. Inheritance Exercise/Person/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if (age <= 15)
+                {
+                    Child child = new(name, age);
+                    Console.WriteLine(child);
+                }
+                else
+                {
+                    Person person = new(name, age);
+                    Console.WriteLine(person);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
