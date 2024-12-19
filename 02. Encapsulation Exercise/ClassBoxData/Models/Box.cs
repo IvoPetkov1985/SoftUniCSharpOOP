5a277747cd9481fc0334d2731cc5d8b42841ed01
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-12-19 14:34:53 +0200

    Task ClassBoxData uploaded.

---
+++ 02. Encapsulation Exercise/ClassBoxData/Models/Box.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+
+namespace ClassBoxData.Models
+{
+    public class Box
+    {
+        private const string InputNumberErrorMessage = "{0} cannot be zero or negative.";
+
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get { return length; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(InputNumberErrorMessage, nameof(Length)));
+                }
+
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(InputNumberErrorMessage, nameof(Width)));
+                }
+
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(InputNumberErrorMessage, nameof(Height)));
+                }
+
+                height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2 * Length * Width + LateralSurfaceArea();
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return 2 * Length * Height + 2 * Width * Height;
+        }
+
+        public double Volume()
+        {
+            return Length * Width * Height;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new();
+
+            builder.AppendLine($"Surface Area - {SurfaceArea():F2}");
+            builder.AppendLine($"Lateral Surface Area - {LateralSurfaceArea():F2}");
+            builder.AppendLine($"Volume - {Volume():F2}");
+
+            return builder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 02. Encapsulation Exercise/ClassBoxData/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using ClassBoxData.Models;
+
+double length = double.Parse(Console.ReadLine());
+double width = double.Parse(Console.ReadLine());
+double height = double.Parse(Console.ReadLine());
+
+try
+{
+    Box box = new(length, width, height);
+
+    Console.WriteLine(box);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
