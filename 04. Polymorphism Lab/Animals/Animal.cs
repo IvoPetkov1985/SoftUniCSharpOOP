11c0cab1a3c71dcd0cd720e983a767379d0a7dca
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-12-24 15:24:55 +0200

    Second task uploaded.

---
+++ 04. Polymorphism Lab/Animals/Animal.cs
@@ -0,0 +1,31 @@
+﻿namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        protected Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set => name = value;
+        }
+
+        public string FavouriteFood
+        {
+            get => favouriteFood;
+            private set => favouriteFood = value;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my favourite food is {FavouriteFood}";
+        }
+    }
+}
