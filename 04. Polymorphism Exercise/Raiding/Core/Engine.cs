9d821deb1238ca319d05ae1a37a3d21b4b74548d
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-12-26 19:08:23 +0200

    Task Raiding uploaded.

---
+++ 04. Polymorphism Exercise/Raiding/Core/Engine.cs
@@ -0,0 +1,64 @@
+﻿using Raiding.Core.Interfaces;
+using Raiding.Factories.Interfaces;
+using Raiding.IO.Interfaces;
+using Raiding.Models.Interfaces;
+
+namespace Raiding.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly IReader reader;
+        private readonly IWriter writer;
+        private readonly IHeroFactory heroFactory;
+        private readonly ICollection<IBaseHero> heroes;
+
+        public Engine(IReader reader, IWriter writer, IHeroFactory heroFactory)
+        {
+            this.reader = reader;
+            this.writer = writer;
+            this.heroFactory = heroFactory;
+            this.heroes = new List<IBaseHero>();
+        }
+
+        public void Run()
+        {
+            int heroesCount = int.Parse(reader.ReadLine());
+
+            while (heroesCount > 0)
+            {
+                string name = reader.ReadLine();
+                string type = reader.ReadLine();
+
+                try
+                {
+                    heroes.Add(heroFactory.Create(type, name));
+                    heroesCount--;
+                }
+                catch (ArgumentException ex)
+                {
+                    writer.WriteLine(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+
+            int bossPower = int.Parse(reader.ReadLine());
+
+            foreach (IBaseHero hero in heroes)
+            {
+                writer.WriteLine(hero.CastAbility());
+            }
+
+            if (heroes.Sum(h => h.Power) >= bossPower)
+            {
+                writer.WriteLine("Victory!");
+            }
+            else
+            {
+                writer.WriteLine("Defeat...");
+            }
+        }
+    }
+}
