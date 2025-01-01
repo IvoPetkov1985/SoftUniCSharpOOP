9483d49efae3961f258a6f77f1143395214b2fc5
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2025-01-01 18:20:23 +0200

    Task DetailPrinter uploaded.

---
+++ 07. SOLID Lab/P03.Detail_Printer/Manager.cs
@@ -0,0 +1,30 @@
+﻿using P03.Detail_Printer.Contracts;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03.DetailPrinter
+{
+    public class Manager : Employee, IEmployee
+    {
+        public Manager(string name, ICollection<string> documents) 
+            : base(name)
+        {
+            this.Documents = new List<string>(documents);
+        }
+
+        public IReadOnlyCollection<string> Documents { get; set; }
+
+        public override string PrintDetails()
+        {
+            StringBuilder builder = new();
+            builder.AppendLine(Name);
+
+            foreach (string document in Documents)
+            {
+                builder.AppendLine(document);
+            }
+
+            return builder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 07. SOLID Lab/P03.Detail_Printer/Program.cs
@@ -0,0 +1,17 @@
+﻿using P03.Detail_Printer.Contracts;
+using System.Collections.Generic;
+
+namespace P03.DetailPrinter
+{
+    class Program
+    {
+        static void Main()
+        {
+            IEmployee employee = new Employee("Peter");
+            IEmployee manager = new Manager("Anna", new List<string>() { "CV", "Schedule", "Working plan" });
+
+            DetailsPrinter printer = new(new List<IEmployee>() { employee, manager });
+            printer.PrintDetails();
+        }
+    }
+}
