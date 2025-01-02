c1cb854c45adc184ca29f1b0f1397930944d165c
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2025-01-02 15:31:29 +0200

    First task SquareRoot uploaded.

---
+++ 05. Exceptions Handling Lab/SquareRoot/Program.cs
@@ -0,0 +1,24 @@
+﻿try
+{
+	ProcessCommand();
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Goodbye.");
+}
+
+static void ProcessCommand()
+{
+	int inputNumber = int.Parse(Console.ReadLine());
+
+	if (inputNumber < 0)
+	{
+		throw new ArgumentException("Invalid number.");
+	}
+
+    Console.WriteLine(Math.Sqrt(inputNumber));
+}
