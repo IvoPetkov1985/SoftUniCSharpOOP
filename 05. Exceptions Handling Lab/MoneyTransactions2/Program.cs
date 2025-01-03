c590b145c8cb0c3a71d4b0437d034fb8ea856166
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2025-01-03 15:46:58 +0200

    Task MoneyTransactions2 uploaded.
    Another solution with objects...

---
+++ 05. Exceptions Handling Lab/MoneyTransactions2/Program.cs
@@ -0,0 +1,97 @@
+﻿ICollection<BankAccount> bankAccounts = new List<BankAccount>();
+
+string accountsInfoLine = Console.ReadLine();
+string[] accountsArr = accountsInfoLine
+    .Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+foreach (string accountInfo in accountsArr)
+{
+    string[] accountTokens = accountInfo
+        .Split("-", StringSplitOptions.RemoveEmptyEntries);
+
+    int accountNum = int.Parse(accountTokens[0]);
+    decimal accountBalance = decimal.Parse(accountTokens[1]);
+
+    BankAccount account = new(accountNum, accountBalance);
+    bankAccounts.Add(account);
+}
+
+string commandLine = Console.ReadLine();
+
+while (commandLine != "End")
+{
+    string[] tokens = commandLine
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    try
+    {
+        ProcessCommand(tokens);
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    catch (InvalidOperationException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    finally
+    {
+        Console.WriteLine("Enter another command");
+    }
+
+    commandLine = Console.ReadLine();
+}
+
+void ProcessCommand(string[] tokens)
+{
+    string command = tokens[0];
+    int accountNumber = int.Parse(tokens[1]);
+
+    BankAccount account = bankAccounts.FirstOrDefault(ba => ba.AccountNumber == accountNumber);
+
+    if (account == null)
+    {
+        throw new ArgumentException("Invalid account!");
+    }
+
+    decimal sum = decimal.Parse(tokens[2]);
+
+    if (command == "Deposit")
+    {
+        account.AccountBalance += sum;
+    }
+    else if (command == "Withdraw")
+    {
+        if (sum > account.AccountBalance)
+        {
+            throw new InvalidOperationException("Insufficient balance!");
+        }
+
+        account.AccountBalance -= sum;
+    }
+    else
+    {
+        throw new ArgumentException("Invalid command!");
+    }
+
+    Console.WriteLine(account.BalanceChangedInfo());
+}
+
+public class BankAccount
+{
+    public BankAccount(int accountNumber, decimal accountBalance)
+    {
+        AccountNumber = accountNumber;
+        AccountBalance = accountBalance;
+    }
+
+    public int AccountNumber { get; private set; }
+
+    public decimal AccountBalance { get; set; }
+
+    public string BalanceChangedInfo()
+    {
+        return $"Account {AccountNumber} has new balance: {AccountBalance:F2}";
+    }
+}
