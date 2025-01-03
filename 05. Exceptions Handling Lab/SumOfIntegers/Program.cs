a8cb1752e6169a6d4a423a3a59d4f2c77d43f0db
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2025-01-03 15:44:31 +0200

    Task SumOfIntegers uploaded.

---
+++ 05. Exceptions Handling Lab/SumOfIntegers/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] inputTokens = Console.ReadLine()
+    .Split(" ");
+
+ICollection<int> ints = new List<int>();
+
+foreach (string token in inputTokens)
+{
+    try
+    {
+        ints.Add(int.Parse(token));
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine($"The element '{token}' is in wrong format!");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine($"The element '{token}' is out of range!");
+    }
+    finally
+    {
+        Console.WriteLine($"Element '{token}' processed - current sum: {ints.Sum()}");
+    }
+}
+
+Console.WriteLine($"The total sum of all integers is: {ints.Sum()}");
